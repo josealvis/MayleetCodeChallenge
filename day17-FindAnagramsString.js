82b195b4565e90c74b89dfc0f97f5325832ff3fc
jose alvis <josealvis91@hotmail.com>
2020-05-17 21:42:41 -0400

    code review

--- day17-FindAnagramsString.js
+++ day17-FindAnagramsString.js
@@ -61,7 +61,7 @@ let findAnagrams = (s, p) => {
 };
 
 // brute force with some improve
-findAnagrams = (s, p) => {
+let findAnagrams2 = (s, p) => {
     if (s == p) return [0];
     if (s.length < p.length) return [];
     const mapP = map(p);
@@ -71,13 +71,13 @@ findAnagrams = (s, p) => {
     for (let x = 0; x < s.length; x++) {
         let sub = s.substr(x, p.length);
         if (sub.length < p.length) break;
-        if (nextTrue || isAnagram(sub,p)) {
+        if (nextTrue || isAnagram(sub, p)) {
             result.push(x);
             if (s[x + p.length] == s[x]) {
                 nextTrue = true;
             } else {
                 nextTrue = false;
-                mapP[s[x + p.length] ] == undefined? x + p.length-1 : x = x + 1;
+                mapP[s[x + p.length]] == undefined ? x + p.length - 1 : x = x + 1;
             }
         }
 
@@ -88,7 +88,7 @@ findAnagrams = (s, p) => {
     function map(text) {
         let count = 0;
         let arr = text.split("").reduce((obj, e) => {
-            if (obj[e]) return obj ;
+            if (obj[e]) return obj;
             else obj[e] = 0;
             return obj;
         }, {})
@@ -96,23 +96,70 @@ findAnagrams = (s, p) => {
         return arr;
     }
 
-    function isAnagram(str1, str2){
-        let count  = map(str2);
-        for(var i =0; str1[i] && str2[i];i++ ){
+    function isAnagram(str1, str2) {
+        let count = map(str2);
+        for (var i = 0; str1[i] && str2[i]; i++) {
             count[str1[i]]++;
             count[str2[i]]--;
         }
 
-        if (str1[i] || str2[i]) 
-        return false; 
+        if (str1[i] || str2[i])
+            return false;
 
-        for (let prop in count){
-        if (count[prop]) return false; 
+        for (let prop in count) {
+            if (count[prop]) return false;
         }
-    return true; 
-    
+        return true;
+
     }
 };
+
+
+
+findAnagrams = (s, p) => {
+    let mapP = p.split("").reduce((obj, e) => {
+        if (obj[e]) obj[e]++;
+        else obj[e] = 1;
+        return obj;
+    }, {});
+
+    let currentMap = { ...mapP };
+    let count = p.length;
+    let start = 0;
+    let end = 0;
+    let result = [];
+
+    while (end < s.length) {
+
+        if (end - start >= p.length) start++;
+        if (currentMap[s[end]] == undefined) {
+            start = end + 1;
+            end = start;
+            count = p.length;
+            currentMap = { ...mapP };
+            continue;
+        }
+        while (currentMap[s[end]] == 0) {
+            currentMap[s[start]] = currentMap[s[start]] + 1;
+            start++;
+            count++;
+        }
+
+        currentMap[s[end]] = currentMap[s[end]] - 1;
+        count--;
+
+        if (count == 0) {
+            result.push(start);
+            count++;
+            currentMap[s[start]] = currentMap[s[start]] + 1;
+            start++;
+        }
+        end++;
+    }
+
+    return result;
+};
+
 
 
 exports.solution = findAnagrams;
